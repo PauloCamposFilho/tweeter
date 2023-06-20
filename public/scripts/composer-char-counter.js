7e338d339e80a00e02e966f341af4c9ea228d84a
Paulo Campos Filho <pezaum@gmail.com>
2023-06-20 13:30:58 -0700

    Refactored tweet form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   // get input field for tweet.
   const inputField = $("#tweet-text");
+  const initialHeight = inputField.height();
   // get input length left-over counter
   const counter = $("#tweet-counter");
   // get send tweet button
@@ -9,13 +10,21 @@ $(document).ready(function() {
   inputField.on("input", function(event) {
     const _field = $(this);
     const currentLength = _field.val().length;
-    const maxLength = _field.attr("data-maxlength");    
-    counter.val(maxLength - currentLength);
+    const maxLength = _field.attr("data-maxlength");
+    
+    counter.val(maxLength - currentLength);    
+    _field.css("height", $(this).prop("scrollHeight") + "px");
+    
+    if (currentLength === 0) {
+      _field.height(initialHeight);
+    }
+    
     if (currentLength > maxLength) {
       sendTweetBtn.addClass("disabled").prop("disabled", true);
       counter.addClass("overflow");
       return;
-    } 
+    }
+    
     sendTweetBtn.removeClass("disabled").prop("disabled", false);
     counter.removeClass("overflow");
   });
