a1a226e84ff9c64e984376d1d6c072aa60307f7d
Paulo Campos Filho <pezaum@gmail.com>
2023-06-20 14:18:37 -0700

    Changed element reference in counter javascript.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   // get input field for tweet.
   const inputField = $("#tweet-text");
-  const initialHeight = inputField.height();
-  // get input length left-over counter
-  const counter = $("#tweet-counter");
+  const initialHeight = inputField.height();  
   // get send tweet button
   const sendTweetBtn = $("#tweet-send");
 
@@ -11,8 +9,8 @@ $(document).ready(function() {
     const _field = $(this);
     const currentLength = _field.val().length;
     const maxLength = _field.attr("data-maxlength");
-    
-    counter.val(maxLength - currentLength);    
+    const _counter = $(this).parent().find(".counter");
+    _counter.val(maxLength - currentLength);    
     _field.css("height", $(this).prop("scrollHeight") + "px");
     
     if (currentLength === 0) {
@@ -21,11 +19,11 @@ $(document).ready(function() {
     
     if (currentLength > maxLength) {
       sendTweetBtn.addClass("disabled").prop("disabled", true);
-      counter.addClass("overflow");
+      _counter.addClass("overflow");
       return;
     }
     
     sendTweetBtn.removeClass("disabled").prop("disabled", false);
-    counter.removeClass("overflow");
+    _counter.removeClass("overflow");
   });
 });
