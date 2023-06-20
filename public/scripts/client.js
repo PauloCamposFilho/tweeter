6161319e7f9f1dd3084ae7e4c7c7fe0621e29d40
Paulo Campos Filho <pezaum@gmail.com>
2023-06-19 21:23:47 -0700

    Added basic javascript to count down how many characters left the user has on a tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  // get input field for tweet.
+  const inputField = $("#tweet-text");
+  // get input length left-over counter
+  const counter = $("#tweet-counter");
+  inputField.on("input", (event) => {
+    const _field = $(event.target);
+    const currentLength = _field.val().length;
+    const maxLength = _field.attr("maxLength");    
+    counter.val(maxLength - currentLength);
+  });
+});
