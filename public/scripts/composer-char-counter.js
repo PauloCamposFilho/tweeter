3613810703662a4d1951bc07e8a09dd11cec3dc8
Paulo Campos Filho <pezaum@gmail.com>
2023-06-21 01:53:04 -0700

    Slightly refactored .counter logic. Added a hard cap of 500 characters on new-tweet textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
   // get input field for tweet.
   const inputField = $("#tweet-text");
-  const initialHeight = inputField.height();
   // get send tweet button
   const sendTweetBtn = $("#tweet-send");
 
-  inputField.on("input", function(event) {
-    const _field = $(this);
-    const currentLength = _field.val().length;
-    const maxLength = _field.attr("data-maxlength");
-    const _counter = $(this).parent().find(".counter");
-    // update the counter
-    _counter.val(maxLength - currentLength); 
-    // update the textarea height (in case it's multiline now)
-    _field.css("height", $(this).prop("scrollHeight") + "px");
+  const updateTextareaHeight = ($textarea) => {
+    $textarea.css("height", "auto");
+    $textarea.css("height", $textarea.prop("scrollHeight") + "px");
+  }
 
-    // if there's nothing in the textarea, reset the height.
-    if (currentLength === 0) {
-      _field.height(initialHeight);
-    }
+  inputField.on("input", function() {
+    const $field = $(this);
+    const currentLength = $field.val().length;
+    const maxLength = $field.data("maxlength");
+    const $counter = $(this).closest(".new-tweet").find(".counter");
+    // update the counter
+    $counter.val(maxLength - currentLength);
+    // update the textarea height based on current input
+    updateTextareaHeight($field);
 
     // add css error/overflow classes if tweet is too long.
     if (currentLength > maxLength) {
       sendTweetBtn.addClass("disabled").prop("disabled", true);
-      _counter.addClass("overflow");
+      $counter.addClass("overflow");
       return;
     }
 
     // remove css error/overflow classes if tweet is fine.
     sendTweetBtn.removeClass("disabled").prop("disabled", false);
-    _counter.removeClass("overflow");
+    $counter.removeClass("overflow");
   });
 });
